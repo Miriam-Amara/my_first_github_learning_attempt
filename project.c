9508cb086b3a50f3d442365f5b814784beab5c72
Miriam Ikechukwu <ikechukwumiriam@gmail.com>
2023-07-26 02:55:34 +0100

    Created project.c today

---
+++ project.c
@@ -0,0 +1,14 @@
+//A software that calculates how many dozens of eggs z company produces in a day
+
+#include <stdio.h>
+
+int main()
+{
+  int eggs;
+  printf("\nHow many eggs did z company produce today\nPlease input the value: ");
+  scanf("%i", &eggs);
+
+
+  
+  return 0;
+}
