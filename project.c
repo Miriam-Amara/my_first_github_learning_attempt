921840b75415144749f027aa29e9c570686eeb57
Miriam Ikechukwu <ikechukwumiriam@gmail.com>
2023-07-26 04:14:34 +0100

    modified project.c

--- project.c
+++ project.c
@@ -8,6 +8,10 @@ int main()
   printf("\nHow many eggs did z company produce today\nPlease input the value: ");
   scanf("%i", &eggs);
 
+  double dozen_eggs = eggs/12.0;
+
+  printf("\nToday, z company produced %lf dozens of eggs\n", dozen_eggs);
+
 
   
   return 0;
